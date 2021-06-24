0efd134a033d8dc17c165106b013b5faec6b66a8
Matthew Sunner <matthew@matthews-air.lan>
2021-06-23 22:04:16 -0500

    Update Docstring for break_even()

--- pyfin/pyfin.py
+++ pyfin/pyfin.py
@@ -30,6 +30,17 @@ def cogs(beg_inv, pur_inv, end_inv):
 
 
 def break_even(fixed_costs, sales_price, variable_cost_per_unit):
+    """break_even: Break Even Point Calculation
+
+    Args:
+        fixed_costs (float): Value of all Fixed Costs
+        sales_price (float): Sales Price of the Item Being Sold
+        variable_cost_per_unit (float): Variable Development Cost Associate with Item Being Sold
+
+    Returns:
+        break_even (float): Break Even Point: Number of Units to Sell to Break Even
+    """
+
     break_even = fixed_costs / (sales_price * variable_cost_per_unit)
 
     return break_even
